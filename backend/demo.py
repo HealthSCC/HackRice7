b6eae21a49c263f65ca1d4cdf768dfa810971ef4
Rena7ssance <roy7wt@gmail.com>
2017-09-23 00:58:47 -0500

    Hello world

---
+++ backend/demo.py
@@ -0,0 +1,35 @@
+from flask import Flask, g
+import sqlite3
+
+app = Flask(__name__)
+DATABASE = "../database/HACKRICE.db"
+
+
+@app.route('/test')
+def hello_world():
+    return "hello world"
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ database/init_db.py
@@ -0,0 +1,45 @@
+import sqlite3
+
+'''
+conn = sq
+lite3.connect('HACKRICE.db')
+cursor = conn.cursor()
+cursor.execute('CREATE TABLE test (id varchar(20) PRIMARY KEY, pw varchar(20))')
+cursor.execute('INSERT INTO test (id, pw) VALUES (\'roy7wt\', \'123456\')')
+cursor.close()
+conn.commit()
+conn.close()
+'''
+
+DATABASE_NAME = 'HACKRICE.db'
+
+
+def get_db():
+    return sqlite3.connect(DATABASE_NAME)
+
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+
+def crud_db(query, args=()):
+    get_db().cursor().execute(query, args)
+    get_db().commit()
+
+
+if __name__ == '__main__':
+    conn = sqlite3.connect(DATABASE_NAME)
+    cur = conn.cursor()
+    args = ('rice@rice.edu.cn', '123456', 'RICE', 'RICE')
+    # cur.execute('insert into patient values (\'rice@rice.edu\',\'123456\',\'RICE\',\'RICE\')')
+    cur.execute('insert into patient values (?,?,?,?)', args)
+    values = cur.execute('select * from patient').fetchall()
+    for value in values:
+        print value
+
+    cur.close()
+    conn.commit()
+    conn.close()
